08991c691c7f7ba0a91cd2e6a1fc397f9e8729b0
pwhite15 <pwhite15@my.apsu.edu>
2020-01-29 12:39:25 -0600

    added commets in main and testing push and pull

--- app/src/main/java/csci/apsu/tictactoe/MainActivity.java
+++ app/src/main/java/csci/apsu/tictactoe/MainActivity.java
@@ -1,8 +1,20 @@
+/*
+Names: Dalton Claxton
+       Daniel Davis
+       Peyton White
+
+Project: TicTacToe
+Assignment 1
+
+ */
+
 package csci.apsu.tictactoe;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import csci.apsu.tictactoe.R;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +22,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+
+
+
     }
 }
